d3373621e465a3a31e2cd1da261dc5b86deb939f
ZouJin <zoujin.dev@exlb.org>
2023-02-16 17:05:35 +1100

    User Login&Register

---
+++ UNSoftWare/Map/User.cs
@@ -0,0 +1,76 @@
+﻿using LinePutScript;
+using UNSoftWare.DataBase;
+using static UNSoftWare.Program;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace UNSoftWare.Map
+{
+    public static class User
+    {
+        /// <summary>
+        /// User Token List
+        /// long -> Token
+        /// int -> Uid
+        /// </summary>
+        public static Dictionary<long, int> Tokens = new Dictionary<long, int>();
+
+        /// <summary>
+        /// Login User and get login user info
+        /// </summary>
+        public static async void Login(HttpContext context)
+        {
+            string uoe = context.Request.Form["idoremail"];
+            long pass = Sub.GetHashCode(context.Request.Form["password"]);
+            MV_User usr;
+            if (int.TryParse(uoe, out int id))
+            {
+                usr = FSQL.Select<MV_User>().Where(a => a.Uid == id && a.Password == pass).First();
+            }
+            else
+            {
+                usr = FSQL.Select<MV_User>().Where(a => a.Email == uoe && a.Password == pass).First();
+            }
+            if (usr == null)
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Email or Password Error");
+            }
+            else
+            {
+                long token = Rnd.NextInt64();
+                Tokens.Add(token, usr.Uid);
+                var jret = new JObject();
+                jret["token"] = token;
+                jret["userinfo"] = JObject.Parse(JsonConvert.SerializeObject(usr));
+                await context.Response.WriteAsync(jret.ToString());
+            }
+        }
+        /// <summary>
+        /// Register User
+        /// </summary>
+        public static async void Register(HttpContext context)
+        {
+            string name = context.Request.Form["name"];
+            string email = context.Request.Form["email"];
+            string pass = context.Request.Form["password"];
+            MV_User usr = FSQL.Select<MV_User>().Where(a => a.Email == email).First();
+            if (usr == null)
+            {
+                usr = new MV_User(name, email, pass);
+                usr.Uid = (int)FSQL.Insert(usr).ExecuteIdentity();
+                long token = Rnd.NextInt64();
+                Tokens.Add(token, usr.Uid);
+                var jret = new JObject();
+                jret["token"] = token;
+                jret["userinfo"] = JObject.Parse(JsonConvert.SerializeObject(usr));
+                await context.Response.WriteAsync(jret.ToString());
+            }
+            else
+            {
+                context.Response.StatusCode = 406;
+                await context.Response.WriteAsync("Email Already exists");
+            }
+        }
+    }
+}
--- UNSoftWare/Program.cs
+++ UNSoftWare/Program.cs
@@ -1,20 +1,24 @@
+using UNSoftWare.Map;
+
 namespace UNSoftWare
 {
     public class Program
     {
         static string connectionString = "Data Source=192.168.1.100;Port=3306;User ID=unsoftw;Password=yI1.ft!@p*;Charset=utf8;Database=3900UNSW;SslMode=none;Max pool size=10";
-
+        public static Random Rnd = new Random();
         public static IFreeSql FSQL = new FreeSql.FreeSqlBuilder()
              .UseConnectionString(FreeSql.DataType.MySql, connectionString)
              .UseMonitorCommand(cmd => Console.WriteLine($"Sql：{cmd.CommandText}"))
-             .UseAutoSyncStructure(false) //自动同步实体结构到数据库
+             .UseAutoSyncStructure(true) //自动同步实体结构到数据库
              .Build(); //请务必定义成 Singleton 单例模式
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.MapGet("/", () => "UNSoftWare BackEND");
+            app.MapPost("/User/Login", User.Login);
+            app.MapPost("/User/Register", User.Register);
 
             app.Run();
         }
