0d699bc9999bcf63f0aca9e1a10c507c1c9a1998
sookieli123 <125941352+sookieli123@users.noreply.github.com>
2023-04-22 04:52:45 +1000

    Update registerpage.js

--- frontend/src/components/registerpage.js
+++ frontend/src/components/registerpage.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import "../styles/Form.css";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { backendurl } from "./backendurl";
 
 const JSONbig = require("json-bigint");
 
