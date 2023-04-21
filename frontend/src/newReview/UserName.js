308582c44e0dc353a96af5a52c68744c3d17a20b
sookieli123 <125941352+sookieli123@users.noreply.github.com>
2023-04-22 04:50:03 +1000

    Update UserName.js

--- frontend/src/newReview/UserName.js
+++ frontend/src/newReview/UserName.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { backendurl } from "../components/backendurl";
 
 function UserName({ uid }) {
   const [userName, setUserName] = useState('');
