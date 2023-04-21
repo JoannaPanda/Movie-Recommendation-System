5906122902dd5ce60a9ae62600ac286706bcd930
sookieli123 <125941352+sookieli123@users.noreply.github.com>
2023-04-22 04:55:45 +1000

    Update loginpage.js

--- frontend/src/components/loginpage.js
+++ frontend/src/components/loginpage.js
@@ -3,6 +3,7 @@ import "../styles/Form.css";
 import { Link } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { backendurl } from "./backendurl";
 
 const JSONbig = require("json-bigint");
 
